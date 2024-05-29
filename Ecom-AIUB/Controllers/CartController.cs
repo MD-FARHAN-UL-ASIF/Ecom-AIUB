c13a3c867d3b81fdf0b381cf04f75a1b6cce41e2
MD-FARHAN-UL-ASIF <farhanchowdhury66@gmail.com>
2024-05-29 14:28:46 +0600

    payment gateway works

--- Ecom-AIUB/Controllers/CartController.cs
+++ Ecom-AIUB/Controllers/CartController.cs
@@ -1,8 +1,12 @@
 ﻿using Ecom_AIUB.EF;
 using Ecom_AIUB.Models;
+using Ecom_AIUB.Models.DTOs;
+using Ecom_AIUB.PaymentGateway;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Nancy;
+using System.Collections.Specialized;
 
 namespace Ecom_AIUB.Controllers
 {
@@ -228,6 +232,115 @@ namespace Ecom_AIUB.Controllers
                 message = "Item not found!"
 
             });
+        }
+
+        [HttpPost]
+        [Route("/checkout")]
+        public async Task<IActionResult> Checkout(UserDTO data)
+        {
+            var email = HttpContext.Session.GetString("email");
+            if(email == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var user = await db.Users.Where(x => x.Email == email).FirstOrDefaultAsync();
+            if (user == null) 
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var carts = await db.Carts.Where(x => x.UserId == user.Id).ToListAsync();
+            if (carts.Count <= 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var productsName = new List<string>();
+
+            decimal price = 0M;
+
+            foreach (var cart in carts)
+            {
+                var product = await db.Products.FirstOrDefaultAsync(p => p.Id == cart.ProductId);
+                if (product != null)
+                {
+                    price += product.Price;
+                    productsName.Add(product.Name);
+                }
+            }
+
+            var trx_id = new Guid();
+
+            var baseUrl = Request.Scheme + "://" + Request.Host;
+
+            // CREATING LIST OF POST DATA
+            NameValueCollection PostData = new NameValueCollection();
+
+            PostData.Add("total_amount", $"{price}");
+            PostData.Add("tran_id", $"{trx_id}");
+            PostData.Add("success_url", baseUrl + "/payment/success");
+            PostData.Add("fail_url", baseUrl + "/Cart/CheckoutFail");
+            PostData.Add("cancel_url", baseUrl + "/Cart/CheckoutCancel");
+
+            PostData.Add("version", "3.00");
+            PostData.Add("cus_name", $"{user.Name}");
+            PostData.Add("cus_email", $"{user.Email}");
+            PostData.Add("cus_add1", $"{data.Address}");
+            PostData.Add("cus_add2", "Address Line Tw");
+            PostData.Add("cus_city", $"{data.City}");
+            PostData.Add("cus_state", "State Nam");
+            PostData.Add("cus_postcode", $"{data.PostCode}");
+            PostData.Add("cus_country", "Bangladesh");
+            PostData.Add("cus_phone", $"{user.PhoneNumber}");
+            PostData.Add("cus_fax", "0171111111");
+            PostData.Add("ship_name", "ABC XY");
+            PostData.Add("ship_add1", "Address Line On");
+            PostData.Add("ship_add2", "Address Line Tw");
+            PostData.Add("ship_city", "City Nam");
+            PostData.Add("ship_state", "State Nam");
+            PostData.Add("ship_postcode", "Post Cod");
+            PostData.Add("ship_country", "Countr");
+            PostData.Add("value_a", $"{user.Id}");
+            PostData.Add("value_b", "ref00");
+            PostData.Add("value_c", "ref00");
+            PostData.Add("value_d", "ref00");
+            PostData.Add("shipping_method", "NO");
+            PostData.Add("num_of_item", $"{carts.Count}");
+            PostData.Add("product_name", $"{productsName}");
+            PostData.Add("product_profile", "general");
+            PostData.Add("product_category", "Demo");
+
+            //we can get from email notificaton
+            var storeId = "bootc6608eba3cb6dc";
+            var storePassword = "bootc6608eba3cb6dc@ssl";
+            var isSandboxMood = true;
+
+            SSLCommerz sslcz = new SSLCommerz(storeId, storePassword, isSandboxMood);
+
+            string response = sslcz.InitiateTransaction(PostData);
+
+            return Redirect(response);
+        }
+
+        [HttpGet]
+        [Route("/payment/success")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/payment/success")]
+        public IActionResult Success(string data) 
+        { 
+            return View(data); 
+        }
+
+        [Route("/payment/fail")]
+        public IActionResult Fail()
+        {
+            return View();
         }
     }
 }
--- Ecom-AIUB/Models/DTOs/UserDTO.cs
+++ Ecom-AIUB/Models/DTOs/UserDTO.cs
@@ -8,5 +8,8 @@
         public string Password { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public int UserType { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostCode { get; set; } = string.Empty;
     }
 }
